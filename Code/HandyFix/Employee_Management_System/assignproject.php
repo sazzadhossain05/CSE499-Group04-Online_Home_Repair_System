e928d45726c30a476e5994e1ca11814b8a889b83
Sazzad Hossain <47280176+sazzadhossain05@users.noreply.github.com>
2021-05-15 23:14:47 +0600

    Add files via upload

---
+++ Code/HandyFix/Employee_Management_System/assignproject.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once ('process/dbh.php');
+$sql = "SELECT * from `project` order by subdate desc";
+
+//echo "$sql";
+$result = mysqli_query($conn, $sql);
+
+?>
+
+
+
+<html>
+<head>
+	<title>Project Status |  Admin Panel | HandyFix Employee Management System</title>
+	<link rel="stylesheet" type="text/css" href="styleview.css">
+</head>
+<body>
+	<header>
+		<nav>
+			
+			<ul id="navli">
+				<li><a class="homeblack" href="aloginwel.php">Home</a></li>
+				<li><a class="homeblack" href="addemp.php">Add Emp.</a></li>
+				<li><a class="homeblack" href="viewemp.php">View Emp.</a></li>
+				<li><a class="homeblack" href="assign.php">Assign Project</a></li>
+				<li><a class="homered" href="assignproject.php">Project Status</a></li>
+				<li><a class="homeblack" href="salaryemp.php">Salary Table</a></li>
+				<li><a class="homeblack" href="empleave.php">Emp. Leave</a></li>
+				<!--<li><a class="homeblack" href="applicationofserviceman.php">Job App.</a></li>-->
+				<li><a class="homeblack" href="alogin.html">Log Out</a></li>
+			</ul>
+		</nav>
+	</header>
+	
+	<div class="divider"></div>
+
+		<table>
+			<tr>
+
+				<th align = "center">Project ID</th>
+				<th align = "center">Employee ID</th>
+				<th align = "center">Project Name</th>
+				<th align = "center">Due Date</th>
+				<th align = "center">Submission Date</th>
+				<th align = "center">Mark</th>
+				<th align = "center">Status</th>
+				<th align = "center">Option</th>
+				
+			</tr>
+
+			<?php
+				while ($employee = mysqli_fetch_assoc($result)) {
+					echo "<tr>";
+					echo "<td>".$employee['pid']."</td>";
+					echo "<td>".$employee['eid']."</td>";
+					echo "<td>".$employee['pname']."</td>";
+					echo "<td>".$employee['duedate']."</td>";
+					echo "<td>".$employee['subdate']."</td>";
+					echo "<td>".$employee['mark']."</td>";
+					echo "<td>".$employee['status']."</td>";
+					echo "<td><a href=\"mark.php?id=$employee[eid]&pid=$employee[pid]\">Mark</a>"; 
+
+				}
+
+
+			?>
+
+		</table>
+		
+	
+</body>
+</html>
