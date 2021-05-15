5353dcd08bb55965737bc493a31306f99b8258bb
Sazzad Hossain <47280176+sazzadhossain05@users.noreply.github.com>
2021-05-15 23:19:12 +0600

    Add files via upload

---
+++ Code/HandyFix/career.php
@@ -0,0 +1,96 @@
+
+
+                
+				<br/><br/><br/><br/>
+				<section class="about-meal">
+                    <div class="container">
+                        <h1 class="section-heading">Career In HandyFix</h1>
+                        <div class="about-meal-wrap flex">
+                             
+                            <div class="flex-1">
+                                  
+                                <h3>A passionate team of employees is the foundation of a successful startup. 
+								They become natural advocates and work tirelessly to see the shared company vision come to life.
+								As in life, anything that starts with a weak foundation will struggle to grow. 
+								If you have the passion to contribute a growing startup than join with us. 
+								To set new standards of ethics and excellence in delivering to our customers superior quality and value-for-money residential 
+								and commercial spaces by employing a team of highly motivated and focused professionals.</h3><br><br><br>
+                               
+                                <hr></hr>
+                                  <h1 class="section-heading">Open Positions</h1><br/>
+                                <p>We’re looking for people to join the team who are as excited as we are to help build the platform that empowers the future generation of creators to be successful online.</p>
+                                <br> 
+								
+
+                                
+                            </div>
+							
+                        </div>
+                    </div>
+                </section>
+				
+				<section class="our-services">
+                    <div class="container">
+                       
+                           <div class="card-wrapper flex">
+                           
+						 <div class="service-card magic-shadow-sm">
+							    <h1 style="font-size: 50px;">4</h1><br/>
+								<h3>Marketing & Business Development</h3>
+                               
+                         </div>
+						  <div class="service-card magic-shadow-sm">
+							    <h1 style="font-size: 50px;">5</h1><br/>
+								<h3>Operation</h3>
+                               
+                         </div>
+						  <div class="service-card magic-shadow-sm">
+							    <h1 style="font-size: 50px;">6</h1><br/>
+								<h3>Call Center</h3>
+                               
+                         </div>
+						  
+					      <div class="service-card magic-shadow-sm">
+							    <h1 style="font-size: 50px;">4</h1><br/>
+								<h3>Technician</h3>
+                               
+                         </div>
+						  <div class="service-card magic-shadow-sm">
+							    <h1 style="font-size: 50px;">5</h1><br/>
+								<h3>Sales</h3>
+                               
+                         </div>
+						  <div class="service-card magic-shadow-sm">
+							    <h1 style="font-size: 50px;">6</h1><br/>
+								<h3>Account & Finance</h3>
+                               
+                         </div>
+							
+					</div>
+						
+						
+						
+                    </div>
+                </section>
+				
+				
+				
+				
+				
+				
+				
+				
+
+                
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    
+</body>
+
+</html>
---
+++ Code/HandyFix/earnmoney.php
@@ -0,0 +1,111 @@
+
+
+                
+				<br><br><br><br>
+				 <section class="about-meal">
+                    <div class="container">
+                        <h1 class="section-heading">Refer and Get Paid</h1>
+                        <div class="about-meal-wrap flex">
+                             
+                            <div class="flex-1">
+                                  
+                                <p>
+                                  Are you a social magnet? Earn some money by telling your friends or family about Handy Fix services.<br>
+
+                                  Get started by creating a Handy Fix user account. Spread the word by sharing your unique referral code through blog posts, 
+                                  articles, emails, Facebook posts and tweets. You’ll get paid for every single service completion through your referral code.</p>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+
+
+                <section class="about-meal">
+                    <div class="container">
+                        
+                        <div class="about-meal-wrap flex">
+                             <h1 >Why Refer us?</h1>
+                            <div class="flex-1">
+                                 <br/>
+                                <p>
+								   <ul>
+                                    <li>Anyone can participate</li><br>
+                                    <li>Easy and hustle free refer process</li><br>
+                                    <li>Referrer get reward point by every successful refer</li><br>
+                                    <li>Referrer can use this points to get discount on Handy Fix services</li><br>
+                                    <li>Referrer can also convert this point to cash</li><br>
+                                    <li>Easy and fast cash out process</li><br>
+									</ul> 
+							    </p>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </section>
+     <!---------------------------------------------->
+	  <section class="our-services">
+                    <div class="container">
+                       
+                           <div class="card-wrapper flex">
+                           
+						 <div class="service-card magic-shadow-sm">
+							   <h2>Become a Referrer</h2><br/>
+								<p>
+                                       It's easy and free to join. Just register with your name,
+									   mobile number and valid Mobile banking number to get start from today. 
+									   Mobile banking number is required for cash out process.<br>
+                                </p>
+                               
+                         </div>
+						  <div class="service-card magic-shadow-sm">
+							     <h2>Refer Someone</h2><br/>
+								 <p>
+                                      Start sharing referral code to others, who is looking for a service. 
+									  Referrer can share his/her refer code to any User through social media or a simple SMS.
+                                  </p>
+                               
+                         </div>
+						  <div class="service-card magic-shadow-sm">
+							   <h2>Earn Reward Point</h2>
+    
+                                  <p>
+                                      User can avail any Handy Fix service by using the referral code. 
+									  For every successful service completion, referrer will receive 10% reward points of the actual bill in his/her accounts
+                                  </p>
+                               
+                         </div>
+						  <div class="service-card magic-shadow-sm">
+							    <h2>Get Paid</h2>
+    
+	                             <p>
+                                    Referrer can convert his/her earned reward points into cash and easily cash out through his/her enlisted mobile banking number.
+									To cash out referrer will require 600 reward points.
+								</p> 
+                         </div>
+					   
+							
+					</div>
+                    </div>
+                </section>
+	 <!------------------------------------------------>
+                <section class="about-meal">
+                    <div class="container">
+                       
+                        <div class="about-meal-wrap flex">
+                             
+                            <div class="flex-1">
+                                   <h4>Reward Point Policy</h4><br/>
+                                <p>
+                                   Referrer will get 10% of RP (reward point) of each successful referred service’s payable amount. 
+								   (e.g.- Mr. X referred AC detergent service, BDT 1000 to Mr. Y. After successful service, Mr. X will get 10% or 300 reward point). 
+								   3 reward points is equivalent to BDT 1. Referrer can withdraw his/her amount, if he/she has minimum 600 reward points,
+								   which is equivalent to BDT 200. User or referrer can make service payment through reward point. 
+								   Only half of total service amount can be adjusted through reward point. 
+                                     </p>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </section>
